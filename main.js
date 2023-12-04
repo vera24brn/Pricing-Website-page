73c3eab73de84a5a7c8490af3314b800ea35cf85
Veronica Brindusescu <veronicabrindusescu@Veronicas-MacBook-Air.local>
2023-12-04 12:08:26 +0100

    1st commit

---
+++ main.js
@@ -0,0 +1,51 @@
+//this code creates an interactive overlay effect based on the movement and size of cards. 
+//The overlay effect is achieved by dynamically updating CSS variables based on the pointer position, and the size of overlay cards is synchronized with the size of the original cards using a ResizeObserver.
+
+console.clear();
+const cardsContainer = document.querySelector(".cards");
+const cardsContainerInner = document.querySelector(".cards__inner");
+const cards = Array.from(document.querySelectorAll(".card"));
+const overlay = document.querySelector(".overlay");
+
+// It calculates the position (x and y) of the pointer relative to the .cards container and sets custom CSS properties on the .overlay element. 
+//These custom properties (--opacity, --x, and --y) are then used in the CSS to create an overlay effect.
+const applyOverlayMask = (e) => {
+  const overlayEl = e.currentTarget;
+  const x = e.pageX - cardsContainer.offsetLeft;
+  const y = e.pageY - cardsContainer.offsetTop;
+
+  overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
+};
+
+const createOverlayCta = (overlayCard, ctaEl) => {
+  const overlayCta = document.createElement("div");
+  overlayCta.classList.add("cta");
+  overlayCta.textContent = ctaEl.textContent;
+  overlayCta.setAttribute("aria-hidden", true);
+  overlayCard.append(overlayCta);
+};
+
+const observer = new ResizeObserver((entries) => {
+  entries.forEach((entry) => {
+    const cardIndex = cards.indexOf(entry.target);
+    let width = entry.borderBoxSize[0].inlineSize;
+    let height = entry.borderBoxSize[0].blockSize;
+
+    if (cardIndex >= 0) {
+      overlay.children[cardIndex].style.width = `${width}px`;
+      overlay.children[cardIndex].style.height = `${height}px`;
+    }
+  });
+});
+
+const initOverlayCard = (cardEl) => {
+  const overlayCard = document.createElement("div");
+  overlayCard.classList.add("card");
+  createOverlayCta(overlayCard, cardEl.lastElementChild);
+  overlay.append(overlayCard);
+  observer.observe(cardEl);
+};
+
+//When the pointer moves, the applyOverlayMask function is called, updating the overlay effect.
+cards.forEach(initOverlayCard);
+document.body.addEventListener("pointermove", applyOverlayMask);
